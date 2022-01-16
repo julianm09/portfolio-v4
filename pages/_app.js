dc28bb74620717347b7e031363b20862f8113fdb
Julian Mayes <juliantmayes@gmail.com>
2022-01-16 15:13:34 -0800

    set budget

--- pages/_app.js
+++ pages/_app.js
@@ -26,23 +26,41 @@ function MyApp({ Component, pageProps }) {
       <Header dark={dark} setDark={setDark} />
 
       <Head>
-        <title>Julian Mayes | Frontend Developer + Designer | Vancouver, BC</title>
+      <title>
+          Julian Mayes | Frontend Developer + Designer | Vancouver, BC
+        </title>
         <meta
-          property="og:title"
+          name="title"
           content="Julian Mayes | Frontend Developer + Designer | Vancouver, BC"
-          key="title"
         />
-
         <meta
           name="description"
           content="My name is Julian, I am a frontend developer providing custom design and development services out of Vancouver, BC."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.julianmayes.com/" />
+        <meta
+          property="og:title"
+          content="Julian Mayes | Frontend Developer + Designer | Vancouver, BC"
         />
         <meta
           property="og:description"
           content="My name is Julian, I am a frontend developer providing custom design and development services out of Vancouver, BC."
         />
+        <meta property="og:image" content="https://reshare-app.s3.us-east-2.amazonaws.com/pfp.png" />
 
-        <meta property="og:image" content={"/pfp.png"} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.julianmayes.com/" />
+        <meta
+          property="twitter:title"
+          content="Julian Mayes | Frontend Developer + Designer | Vancouver, BC"
+        />
+        <meta
+          property="twitter:description"
+          content="My name is Julian, I am a frontend developer providing custom design and development services out of Vancouver, BC."
+        />
+        <meta property="twitter:image" content="https://reshare-app.s3.us-east-2.amazonaws.com/pfp.png" />
 
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="stylesheet" href="../styles/globals.css" />
